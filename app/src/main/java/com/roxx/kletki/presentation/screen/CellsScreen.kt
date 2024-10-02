90c96f89ddfd02e3b880fa85c93aa7d410e89383
MaksRoxx <maksimusroxx@gmail.com>
2024-10-02 21:45:43 +0300

    + animation

--- app/src/main/java/com/roxx/kletki/presentation/screen/CellsScreen.kt
+++ app/src/main/java/com/roxx/kletki/presentation/screen/CellsScreen.kt
@@ -7,13 +7,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -26,62 +28,74 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.roxx.kletki.ui.theme.BackgroundColor
 import com.roxx.kletki.ui.theme.ButtonColor
 import com.roxx.kletki.ui.theme.LocalSpacing
+import kotlinx.coroutines.delay
 
 @Composable
 fun CellsScreen(
     viewModel: CellsScreenViewModel = hiltViewModel()
 ) {
     val cells = viewModel.cells.collectAsState()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                Brush.verticalGradient(
-                    listOf(
-                        BackgroundColor,
-                        Color.Black
-                    )
-                )
-            )
-            .padding(LocalSpacing.current.medium)
-    ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            item {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = LocalSpacing.current.medium),
-                    text = "Клеточное наполнение",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp,
-                    color = Color.White,
-                    textAlign = TextAlign.Center
-                )
-            }
-            items(cells.value) {
-                CellItem(it)
-            }
-        }
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-                .clip(RoundedCornerShape(5.dp))
-                .align(Alignment.BottomCenter),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = ButtonColor,
-                contentColor = Color.White
-            ),
-            onClick = { viewModel.addNewCell() }
-        ) {
-            Text(
-                text = "Сотворить",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
 
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(cells.value.size) {
+        if (cells.value.isNotEmpty()) {
+            delay(200L)
+            listState.animateScrollToItem(cells.value.size - 1)
+        }
     }
+
+    Scaffold(
+        bottomBar = {
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(LocalSpacing.current.medium)
+                    .clip(RoundedCornerShape(5.dp)),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = ButtonColor,
+                    contentColor = Color.White
+                ),
+                onClick = { viewModel.addNewCell() }
+            ) {
+                Text(text = "Сотворить")
+            }
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            listOf(
+                                BackgroundColor,
+                                Color.Black
+                            )
+                        )
+                    )
+                    .padding(innerPadding)
+            ) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    state = listState
+                ) {
+                    item {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = LocalSpacing.current.medium),
+                            text = "Клеточное наполнение",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    items(cells.value) { cell ->
+                        CellItem(cell)
+                    }
+                }
+            }
+        }
+    )
 }
